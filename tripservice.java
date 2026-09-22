566e9ac0619d82f98d9710745b50a859aeddb05e
M1NKW <Minh124nb@gmail.com>
2026-09-22 23:27:38 +0700

    moi2

--- tripservice.java
+++ tripservice.java
@@ -1,10 +1,11 @@
 public class tripservice {
     
     public static void main(String[] args) {
-        trip trip1 = new trip("John Doe", "john.doe@example.com", "123-456-7890", "Paris");
+        trip trip1 = new trip("John Doe", "john.doe@example.com", "123-456-7890", "Paris", 1);
         System.out.println("Name: " + trip1.getName());
         System.out.println("Email: " + trip1.getEmail());
         System.out.println("Phone: " + trip1.getPhone());
         System.out.println("Destination: " + trip1.getDestination());
+        System.out.println("Trip Count: " + trip1.getTripCount());
     }
 }
