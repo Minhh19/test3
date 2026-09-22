617f5045033b560c7c1c331efbfebc4154d62ccd
Minato <Minato>
2026-09-22 23:30:41 +0700

    up

--- tripcontroller.java
+++ tripcontroller.java
@@ -1,6 +1,6 @@
 public class tripcontroller {
     public static void main(String[] args) {
-        trip trip1 = new trip("John Doe", "john.doe@example.com", "123-456-7890", "Paris");
+        trip trip1 = new trip("quoc hung", "john@example.com", "123-456-7890", "Ha noi");
 
         System.out.println("Name: " + trip1.getName());
         System.out.println("Email: " + trip1.getEmail());
